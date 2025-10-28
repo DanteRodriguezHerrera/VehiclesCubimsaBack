3ccda3714a9d3982a396a70f3f633db069d7e709
Dante Rodriguez <papikra222@gmail.com>
2025-10-27 19:56:32 -0600

    Enable cors

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! on 3000';
+    return 'Hello World! on 3000 Prueba vehículos';
   }
 }
